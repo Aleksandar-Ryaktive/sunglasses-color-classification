05dc012561c5a5d1185aae2dc7847b2479d5eeb5
Aleksandar <aleksandar@ryaktive.com>
2021-07-26 10:08:18 +0200

    add file_uploader.py

--- file_uploader.py
+++ file_uploader.py
@@ -2,6 +2,11 @@ import streamlit as st
 from PIL import Image
 import prediction as pr
 
+
+st.title("Sunglasses Lens Color Image Classification App")
+st.write("")
+
+
 file_up = st.file_uploader("Upload an image", type="jpg")
 
 image = Image.open(file_up)
