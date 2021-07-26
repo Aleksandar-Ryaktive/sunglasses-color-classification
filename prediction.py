4be8a14d3bfef58058dcadcbc3069a12f510be84
Aleksandar <aleksandar@ryaktive.com>
2021-07-26 21:03:48 +0200

    add prediction

--- prediction.py
+++ prediction.py
@@ -9,7 +9,7 @@ from io import BytesIO
 
 
 #best_model = load_model("C:/Users/Lenovo ThinkPad E15/OneDrive - Ryaktive Software Development/Documents/sunglasses-color-classification/best_model.pt")
-best_model = load_model("https://drive.google.com/drive/folders/1DDk5L_-78PqcWlkMwDoNyaT91B28IhOU?usp=sharing")
+best_model = load_model("https://drive.google.com/drive/folders/1DDk5L_-78PqcWlkMwDoNyaT91B28IhOU?")
 
 
 class_names_processed = ['black',
