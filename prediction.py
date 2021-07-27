c87d7f90e8b0c7a7c2ca073ac860edf311459a19
Aleksandar <aleksandar@ryaktive.com>
2021-07-27 16:44:40 +0200

    add

--- prediction.py
+++ prediction.py
@@ -7,8 +7,8 @@ from tensorflow.keras.applications.imagenet_utils import preprocess_input
 from PIL import Image
 from io import BytesIO 
 
-def load_model():
-    save_dest = Path('best_model')
+def best_model():
+    save_dest = Path('model')
     save_dest.mkdir(exist_ok=True)
     
     f_checkpoint = Path("C:/Users/Lenovo ThinkPad E15/OneDrive - Ryaktive Software Development/Documents/sunglasses-color-classification/best_model.pt")
@@ -17,9 +17,9 @@ def load_model():
         from GD_download import download_file_from_google_drive
         download_file_from_google_drive(cloud_model_location, f_checkpoint)
         
-    best_model = tf.keras.models.load_model(f_checkpoint, map_location=device)
-    best_model.eval()
-    return best_model
+    model = tf.keras.models.load_model(f_checkpoint, map_location=device)
+    model.eval()
+    return model
  
     
 #best_model = load_model("C:/Users/Lenovo ThinkPad E15/OneDrive - Ryaktive Software Development/Documents/sunglasses-color-classification/best_model.pt")
@@ -48,13 +48,13 @@ def read_image(image_encoded):
 
 def get_prediction(image):
     image = np.expand_dims(image, axis=0)
-    prediction = best_model.predict(image)
+    prediction = model.predict(image)
     predicted_class = np.argmax(prediction)
     return class_names_processed[predicted_class]
 
 def url_to_image_(url):
-    #resp = urllib.request.urlopen(url)
-    #image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     image_ = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image_resized = preprocess_input(cv2.resize(image, dsize=(224,224)))
