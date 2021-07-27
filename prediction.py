02e4efa5542b2b89162cb1461a5817ec6991be4d
Aleksandar <aleksandar@ryaktive.com>
2021-07-27 16:33:24 +0200

    add prediction.py

--- prediction.py
+++ prediction.py
@@ -7,9 +7,23 @@ from tensorflow.keras.applications.imagenet_utils import preprocess_input
 from PIL import Image
 from io import BytesIO 
 
-
+def load_model():
+    save_dest = Path('model')
+    save_dest.mkdir(exist_ok=True)
+    
+    f_checkpoint = Path("C:/Users/Lenovo ThinkPad E15/OneDrive - Ryaktive Software Development/Documents/sunglasses-color-classification/best_model.pt")
+    
+    if not f_checkpoint.exists():
+        from GD_download import download_file_from_google_drive
+        download_file_from_google_drive(cloud_model_location, f_checkpoint)
+        
+    model = torch.load(f_checkpoint, map_location=device)
+    model.eval()
+    return model
+ 
+    
 #best_model = load_model("C:/Users/Lenovo ThinkPad E15/OneDrive - Ryaktive Software Development/Documents/sunglasses-color-classification/best_model.pt")
-best_model = load_model("https://drive.google.com/drive/folders/1DDk5L_-78PqcWlkMwDoNyaT91B28IhOU?")
+#best_model = load_model("https://drive.google.com/drive/folders/1DDk5L_-78PqcWlkMwDoNyaT91B28IhOU?export=download")
 
 
 class_names_processed = ['black',
